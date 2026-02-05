9503278d46266535179174ffb51dedd8b100ca0f
Ste <stedamb@gmail.com>
2026-02-05 22:02:18 +0100

    build

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           <div className="container mx-auto px-4">
               <h2 className="text-3xl font-serif font-bold mb-4">Invest in Timeless Value</h2>
               <p className="text-muted-foreground max-w-2xl mx-auto mb-8">
-                  Join our exclusive community of numismatists and secure your legacy with the finest ancient coins available on the market.
+                 Unisciti alla nostra community di numismatici e assicurati un futuro con le migliori monete antiche disponibili sul mercato.
               </p>
           </div>
       </section>
