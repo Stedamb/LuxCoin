4f8b07f295f219ff3aa6b5a24a9a9d0566f9cb21
Ste <stedamb@gmail.com>
2026-03-08 11:54:47 +0100

    feat: Add Cookiebot Cookie Declaration script to the privacy policy page.

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -41,6 +41,12 @@ export default async function PrivacyPage() {
               <PortableTextRenderer value={settings.privacyPolicy} />
             </div>
           )}
+          <script
+            id="CookieDeclaration"
+            src="https://consent.cookiebot.com/9009693e-5045-4c2e-aeb1-2d992d9942ca/cd.js"
+            type="text/javascript"
+            async
+          ></script>
         </div>
       </section>
     </main>
